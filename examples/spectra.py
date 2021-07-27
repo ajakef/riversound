63698d07b33a0651644064c92bbf5a2362e48634
Jake Anderson <ajakef@gmail.com>
2021-07-27 19:39:36 -0400

    improved examples/spectra.py with read_audiomoth and mean spectrum

--- examples/spectra.py
+++ examples/spectra.py
@@ -6,10 +6,15 @@ from scipy.ndimage import median_filter
 from scipy.stats import kurtosis, skew
 import riversound
 
-#%% Plot spectrum of river data
+#%% Read a river .mseed file recorded by a Gem
 ## replace this file name with a file on your computer
-st = obspy.read('/home/jake/Work/StreamAcoustics/BoiseRiver/2021_data/2021-04-10_AnnMorrison/mseed/2021-04-01T00_00_00..150..HDF.mseed')
-st.filter('highpass', freq = 5.0)
+#tr = obspy.read('/home/jake/Work/StreamAcoustics/BoiseRiver/2021_data/2021-04-10_AnnMorrison/mseed/2021-04-01T00_00_00..150..HDF.mseed')[0]
+#tr.filter('highpass', freq = 5.0) # filter out low-frequency noise below 5 Hz
+
+## optional: comment out the last line and replace it with this to read an
+## audiomoth .wav file instead
+tr = riversound.read_audiomoth('/home/jake/Dropbox/riversound/data/20210414_060000.WAV')
+
 
 ## These optional lines trim the data to a user-designated time interval. If
 ## left commented, all the data from the mseed is processed.
@@ -17,22 +22,34 @@ st.filter('highpass', freq = 5.0)
 #duration_hours = 1
 #st.trim(t1, t1 + duration_hours * 3600)
 
+#%% Plot spectrum of river data
+
 ## Calculate the spectrum using a moderately severe kurtosis threshold
-spectrum_dict = riversound.spectrum(st[0], kurtosis_threshold = 0.25)
+spectrum_dict = riversound.spectrum(tr, kurtosis_threshold = 0.25)
+
+## plot spectrogram; white bars are time intervals rejected by kurtosis criterion
+plt.figure()
 riversound.image(np.log10(spectrum_dict['specgram'].T), spectrum_dict['times'], spectrum_dict['freqs'])
-plt.xlabel('Time (sec) after %s' % st[0].stats.starttime.strftime(format = '%Y-%m-%d %H:%M'))
+plt.xlabel('Time (sec) after %s' % tr.stats.starttime.strftime(format = '%Y-%m-%d %H:%M'))
 plt.ylabel('Frequency (Hz)')
 plt.title('Spectrogram')
 
+## plot average spectrum
+plt.figure()
+plt.loglog(spectrum_dict['freqs'], spectrum_dict['mean'])
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Power ($counts^2$)')
+plt.title('Mean Spectrum')
+           
 #%% kurtosis_threshold demo
 ## Kurtosis is a good but not perfect way to identify windy time intervals and
 ## exclude them from the spectrum calculation. A typical kurtosis_threshold
 ## would be around 0.25 to 0.5; 2000 is intended to be an impossible bar to
 ## meet, so no data are excluded based on kurtosis.
 plt.close('all')
-spectrum_dict = riversound.spectrum(st[0], kurtosis_threshold = 2000)
+spectrum_dict = riversound.spectrum(tr, kurtosis_threshold = 2000)
 plt.figure(); plt.semilogy(spectrum_dict['specgram'][100:,:].sum(0), '.')
-spectrum_dict = riversound.spectrum(st[0], kurtosis_threshold = 0.25)
+spectrum_dict = riversound.spectrum(tr, kurtosis_threshold = 0.25)
 plt.figure(); plt.semilogy(spectrum_dict['specgram'][100:,:].sum(0), '.')
 
 #%% runmed_radius_t demo
@@ -40,7 +57,7 @@ plt.figure(); plt.semilogy(spectrum_dict['specgram'][100:,:].sum(0), '.')
 ## when the mean of non-outliers is different from the median. Experiment with
 ## this to make sure it does what you want.
 plt.close('all')
-spectrum_dict = riversound.spectrum(st[0])
+spectrum_dict = riversound.spectrum(tr)
 plt.figure();
 plt.subplot(2,1,1)
 plt.semilogy(spectrum_dict['specgram'][200:400,:].sum(0), '.')
@@ -48,7 +65,7 @@ plt.subplot(2,1,2)
 riversound.image(np.log10(spectrum_dict['specgram'].T), spectrum_dict['times'], spectrum_dict['freqs'])
 
 
-spectrum_dict = riversound.spectrum(st[0], runmed_radius_t = 1, runmed_radius_f = 1)
+spectrum_dict = riversound.spectrum(tr, runmed_radius_t = 1, runmed_radius_f = 1)
 plt.figure();
 plt.subplot(2,1,1)
 plt.semilogy(spectrum_dict['specgram'][200:400,:].sum(0), '.')
