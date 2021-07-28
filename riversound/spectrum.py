fcedc8a99ebb55ebf1f2f27593e026dab4599f83
Jake Anderson <ajakef@gmail.com>
2021-07-27 20:05:48 -0400

    added help docstring to riversound.spectrum

--- riversound/spectrum.py
+++ riversound/spectrum.py
@@ -5,7 +5,59 @@ from scipy.signal import spectrogram
 from scipy.ndimage import median_filter
 from scipy.stats import kurtosis, skew
 
-def spectrum(tr, criterion_function = 'default', runmed_radius_t = 0, runmed_radius_f = 0, nfft = 1024, overlap = 0.5, kurtosis_threshold = 0.5, window = 'hamming'):
+def spectrum(tr, criterion_function = 'default', runmed_radius_t = 0,
+             runmed_radius_f = 0, nfft = 1024, overlap = 0.5,
+             kurtosis_threshold = 0.5, window = 'hamming'):
+    """Calculate spectrogram and Welch spectrum of an obspy trace, where either
+    kurtosis thresholds or median filters can be used to exclude noisy times.
+
+    Parameters
+    ----------
+    tr : obspy.Trace
+        Data trimmed to period for which spectrum is calculated
+    criterion_function : function, 'default', or None
+        Function that returns True or False and determines whether a time window
+        is too noisy to process (False) or acceptable (True). If 'default', test
+        whether the time window's kurtosis is less than kurtosis_threshold. If 
+        None, do not exclude any time windows.
+    runmed_radius_t : int
+        Radius of median filter in time dimension. If zero, do not apply a median
+        filter over time.
+    runmed_radius_f : int
+        Radius of median filter in frequency dimension. If zero, do not apply a 
+        median filter over frequency.
+    nfft: int
+        Length of fft to calculate. The minimum nonzero frequency in the output
+        spectrum will be 1/(nfft * dt).
+    overlap : float
+        Proportion of overlap between adjacent time windows; must be >=0 and <1.
+    kurtosis_threshold : float
+        If using criterion_function = 'default', time windows with kurtosis 
+        greater than this value are considered noisy and excluded.
+    window : str
+        Window function to use when calculating individual spectral estimates. 
+        See help(scipy.signal.windows.getwindow) for options. Common choices
+        include 'hamming', 'hann', 'blackmanharris', and 'boxcar' (rectangular).
+    
+    Returns
+    -------
+    Dictionary with following items:
+    specgram: spectrogram of time period
+    times: times of output spectrogram
+    freqs: frequencies of output spectrum and spectrogram
+    mean: mean spectrum (Welch's method)
+    median: median spectrum
+    stdev: standard deviation of mean spectrum
+
+    Example
+    -------
+    import obspy, riversound
+    import matplotlib.pyplot as plt
+    st = obspy.read()
+    spec_dict = riversound.spectrum(st[0])
+    plt.loglog(spec_dict['freqs'], spec_dict['mean'])
+
+    """
     if criterion_function == 'default':
         def criterion_function(x): return kurtosis(x) < kurtosis_threshold
     
