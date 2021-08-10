75dc892c6e909a01ce8d017e23b887ac7f796369
Taylor Tatum <taylortatum01@gmail.com>
2021-08-10 15:22:27 -0600

    Fixed bug with backslashes in path.

--- riversound/wav.py
+++ riversound/wav.py
@@ -21,7 +21,7 @@ def write_wav(tr, filename = None, time_format = '%Y-%m-%dT%H_%M_%S', path = '.'
 
 def read_audiomoth(filename, network = '', station = '', location = '', channel = 'GDF'):
     # channel code: G means sample rate 1000-5000 Hz, corner period < 10 sec. This is the closest SEED code allowed; it does not support fs > 5000 Hz.
-    datetime_str = filename.split('/')[-1].split('.')[0]
+    datetime_str = filename.split('/')[-1].split('\\')[-1].split('.')[0]
     #(date_str, time_str) = datetime_str.split('_')
     #starttime = obspy.UTCDateTime(date_str[0:4] + '-' + date_str[4:6] + '-' + date_str[6:8] + \
     #                              'T' + time_str[0:2] + ':' + time_str[2:4] + ':' + time_str[4:6])
