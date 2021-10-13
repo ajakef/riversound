ee126cbc2cf9c7e08ab29826691e6567b99f1cf6
Scott <scott.gauvain@gmail.com>
2021-10-12 23:50:38 -0600

    fixed emacs header causing error

--- examples/jumpcrk_spectrogram_joint.py
+++ examples/jumpcrk_spectrogram_joint.py
@@ -1,6 +1,4 @@
-x;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
+
 
 import riversound, glob, obspy, matplotlib, datetime
 import numpy as np
